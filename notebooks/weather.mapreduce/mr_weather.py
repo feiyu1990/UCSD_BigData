d1044113fd3d3fb760386fcedd6c0fa464e88ff2
Ubuntu <ubuntu@ip-172-31-33-175.ec2.internal>
2014-05-27 02:39:06 +0000

    modified:   notebooks/weather.mapreduce/Description of Assignment.ipynb 	modified:   notebooks/weather.mapreduce/mr_weather.py 	modified:   notebooks/weather.mapreduce/weather_MRjob-with-utils.ipynb 	modified:   notebooks/weather.mapreduce/weather_MRjob.ipynb 	notebooks/mrjob/find_waiting_flow.py 	notebooks/mrjob/find_waiting_flow.pyc 	notebooks/weather.mapreduce/PCA_counts 	notebooks/weather.mapreduce/PCA_result 	notebooks/weather.mapreduce/coding.pyc 	notebooks/weather.mapreduce/counts 	notebooks/weather.mapreduce/mr_weather_PCA.py 	notebooks/weather.mapreduce/my_hw.ipynb 	notebooks/weather.mapreduce/my_hw2.ipynb 	notebooks/weather.mapreduce/my_hw2_totaldays.ipynb 	notebooks/weather.mapreduce/my_hw3.ipynb 	notebooks/weather.mapreduce/my_hw_totaldays.ipynb 	notebooks/weather.mapreduce/new_stations_with_minmax.txt 	notebooks/weather.mapreduce/new_stations_with_total_days.txt 	notebooks/weather.mapreduce/stations_with_minmax 	notebooks/weather.mapreduce/stations_with_minmax.pkl 	notebooks/weather.mapreduce/stations_with_total_days 	notebooks/weather.mapreduce/stations_with_totaldays.pkl 	utils/AWS_keypair_management.pyc 	utils/find_waiting_flow.pyc

--- notebooks/weather.mapreduce/mr_weather.py
+++ notebooks/weather.mapreduce/mr_weather.py
@@ -2,38 +2,78 @@
 """
 count the number of measurements of each type
 """
+import sys
+sys.path.append('/usr/lib/python2.6/dist-packages')
 from mrjob.job import MRJob
 import re
 from sys import stderr
 
+WORD_RE = re.compile(r"[\w']+")
+
 #logfile=open('log','w')
-logfile=stderr
+#logfile=stderr
 
 class MRWeather(MRJob):
 
-    def mapper(self, _, line):
-        self.increment_counter('MrJob Counters','mapper',1)
-        elements=line.split(',')
-        if elements[0]=='station':
-            yield('header',1)
-        else:
-            yield(elements[1],1)
-            
-    def combiner(self, word, counts):
-        self.increment_counter('MrJob Counters','combiner',1)
-        yield (word, sum(counts))
-        #l_counts=[c for c in counts]  # extract list from iterator
-        #S=sum(l_counts)
-        #logfile.write('combiner '+word+' ['+','.join([str(c) for c in l_counts])+']='+str(S)+'\n')
-        #yield (word, S)
+    def my_mapper(self, _, line):
+        try:
+            self.increment_counter('MrJob Counters','my_mapper',1)
+            elements=line.split(',')
+            #logfile.write('%s\n' % type(line))
+            if elements[1]=='TMAX' or elements[1]=='TMIN':
+                measure = 1
+                key = (elements[0],elements[2])
+            else:
+                key = ('NA','NA')
+                measure = 0
+            n_days = sum([e!='' for e in elements[3:]])
 
-    def reducer(self, word, counts):
-        self.increment_counter('MrJob Counters','reducer',1)
-        yield (word, sum(counts))
-        #l_counts=[c for c in counts]  # extract list from iterator
-        #S=sum(l_counts)
-        #logfile.write('reducer '+word+' ['+','.join([str(c) for c in l_counts])+']='+str(S)+'\n')
-        #yield (word, S)
+        except Exception, e:
+            stderr.write('Error in line:\n'+line)
+            stderr.write(e)
+            self.increment_counter('MrJob Counters','mapper-error',1)
+            key = ('error','error')
+            measure = 0
+            n_days = 0
+
+        finally:
+            yield key, (measure, n_days)
+
+            
+    def my_combiner(self, word, counts):
+        self.increment_counter('MrJob Counters','my_combiner',1)
+        sum1=0
+        sum2=0
+        for measure, n_days in counts:
+            sum1 = sum1 + measure
+            sum2 = sum2 + n_days
+        yield word, (sum1,sum2)
+
+    def my_reducer1(self, word, counts):
+        self.increment_counter('MrJob Counters','my_reducer1',1)
+        sum1 = 0
+        sum2 = 0
+        for measure, n_days in counts:
+            sum1 = sum1 + measure
+            sum2 = sum2 + n_days
+        if sum1 ==2:
+            yield word[0], (word[1], sum2)
+        else:
+            yield ('NA'), ('NA', 0)
+    def my_reducer2(self, word, counts):
+        self.increment_counter('MrJob Counters','my_reducer2',1)
+        n_year = 0
+        for year, n_days in counts:
+            if n_days>0:
+                n_year= n_year+1
+        if n_year == 0:
+            yield 'NA', 0
+        else:
+            yield word, n_year
+            
+    def steps(self):
+        return [self.mr(mapper=self.my_mapper, combiner=self.my_combiner, reducer = self.my_reducer1),
+                self.mr(reducer=self.my_reducer2)]
 
 if __name__ == '__main__':
     MRWeather.run()
